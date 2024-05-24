a67947c8e10d6cb0ed45c6f6f78dba6f78810022
Denez <denez.guerx@gmail.com>
2024-05-24 16:27:42 +0200

    Python_rag

---
+++ backend.py
@@ -0,0 +1,26 @@
+import argparse
+from langchain.vectorstores.chroma import Chroma
+
+
+from get_embedding_function import get_embedding_function
+
+from fastapi import FastAPI
+CHROMA_PATH = "chroma"
+app = FastAPI()
+
+
+@app.get("/query/{query_text}")
+async def query_rag(query_text: str):
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB. Peut etre que le k est à mettre en parametre
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    score = [_score for doc, _score in results]
+    formatted_response = f"Response: {context_text}\nSources: {sources}\nScore: {score}"
+    return formatted_response
+async def root():
+    return {"message": "Vous n'avez pas passez la requete en param"}
---
+++ test_rag.py
@@ -0,0 +1,58 @@
+from query_data import query_rag
+from langchain_community.llms.ollama import Ollama
+
+EVAL_PROMPT = """
+Expected Response: {expected_response}
+Actual Response: {actual_response}
+---
+(Answer with 'true' or 'false') Does the actual response match the expected response? 
+"""
+
+def main():
+    test_monopoly_rules()
+    test_ticket_to_ride_rules()
+
+
+
+def test_monopoly_rules():
+    assert query_and_validate(
+        question="Combien y'a t-il d'établissements actifs à Portivechju?",
+        expected_response="5934",
+    )
+
+
+def test_ticket_to_ride_rules():
+    assert query_and_validate(
+        question="Le secteur de l'information et de la communication regroupe combien d'établissements participant au système productif à Portivechju?",
+        expected_response="50",
+    )
+
+
+def query_and_validate(question: str, expected_response: str):
+    response_text = query_rag(question)
+    prompt = EVAL_PROMPT.format(
+        expected_response=expected_response, actual_response=response_text
+    )
+
+    model = Ollama(model="mistral")
+    evaluation_results_str = model.invoke(prompt)
+    evaluation_results_str_cleaned = evaluation_results_str.strip().lower()
+
+    print(prompt)
+
+    if "true" in evaluation_results_str_cleaned:
+        # Print response in Green if it is correct.
+        print("\033[92m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return True
+    elif "false" in evaluation_results_str_cleaned:
+        # Print response in Red if it is incorrect.
+        print("\033[91m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return False
+    else:
+        raise ValueError(
+            f"Invalid evaluation result. Cannot determine if 'true' or 'false'."
+        )
+
+
+if __name__ == "__main__":
+    main()
